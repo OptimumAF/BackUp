045eb4860ad2fe57a8af04e4e98eb9a75af99fdd
Avery Fernandez <88298311+OptimumAF@users.noreply.github.com>
2022-02-08 17:07:13 -0600

    Add files via upload

---
+++ Box Print.java
@@ -0,0 +1,13 @@
+class Main {
+	public static void box() {
+		System.out.println("/-------\\");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("\\-------/\n\n");
+	}
+  public static void main(String[] args) {
+    box();
+	box();
+	box();
+  }
+}
