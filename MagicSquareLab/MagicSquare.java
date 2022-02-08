b21b6aa79d636069cd803f8fa4a03275385ed4dc
Avery Fernandez <88298311+OptimumAF@users.noreply.github.com>
2022-02-08 17:15:05 -0600

    Add files via upload

---
+++ MagicSquareLab/MagicSquare.java
@@ -0,0 +1,49 @@
+public class MagicSquare{
+		/* A magic square is a square in which the sum of each row, column, and main diagonal are equal.
+
+    private int[][] square; // n x n 2D array
+    private int n; // number of rows(or columns) of square
+    /** write a constructor that accepts a 2d array
+        Precondition: Assume that square is a square(number of rows = number of columns) 2D array. Assume also that the values in square are in the range 1 - n^2. 
+    */
+    public MagicSquare(int[][] a){
+
+    }
+
+
+
+    /** Returns row sum of the row given by the parameter r
+        Precondition: 0 <= r < number of rows of square
+    */
+    public int rowSum(int r){
+
+    }
+    /** Returns column sum of the column given by the parameter c
+        Precondition: 0 <= c < number of columns of square
+    */
+    public int colSum(int c){
+
+    }
+    /**
+    Returns whether both the diagonal sums are equal to the parameter sum. 
+    */
+    public boolean diagSums(int sum){
+
+    }
+    /**
+    Precondition: Values of square is in range 1 to n^2.
+    Returns true if the numbers 1 to n^2 occurs exactly once in square and false otherwise. 
+    */
+    public boolean exactlyOnce(){
+
+
+    }
+    /**
+    Using all of the helper methods above. Implement isMagic() below so that
+    it returns true if square is magic and false otherwise.  
+    */
+    public boolean isMagic(){
+
+    }
+
+}
---
+++ MagicSquareLab/Main.java
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+        // use initializer list to create the magic square as follows:
+        // 16,3,2,13
+        // 5,10,11,8
+        // 9,6,7,12
+        // 4,15,14,1
+        int[][] magic = {{16,3,2,13},{5,10,11,8}, {9,6,7,12},{4,15,14,1}};
+
+        
+
+        // create an instance of the MagicSquare class, call it a.
+        MagicSquare a = new MagicSquare(magic);
+
+        System.out.println(a.rowSum(0)); // 34
+        // similarly test colSum, diagSums and exactlyOnce
+        // feel free to modify the above 2d array temporarily to test. 
+
+
+        // print out the result of calling isMagic on the instance above. 
+        // Should print true if done correctly.
+      
+        System.out.println(a.isMagic());
+  }
+}
