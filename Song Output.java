d4a0ac8943400a848cd181ed686ad7dec14c8c1a
Avery Fernandez <88298311+OptimumAF@users.noreply.github.com>
2022-02-08 17:06:13 -0600

    Add files via upload

---
+++ Song Output.java
@@ -0,0 +1,126 @@
+class Main {
+	public static void Title(){
+		System.out.println("Death Bed by Powfu and Beabadoobee");
+		Line();
+	}
+	public static void Line(){
+		System.out.println("\n");
+	}
+	public static void Chorus(){
+		System.out.println("Don't stay awake for too long");
+		System.out.println("Don't go to bed");
+		System.out.println("I'll make a cup of coffee for your head");
+		System.out.println("I'll get you up and going out of bed");
+		System.out.println("And I, don't stay awake for too long");
+		System.out.println("Don't go to bed");
+		System.out.println("I'll make a cup of coffee for your head");
+		System.out.println("I'll get you up and going out of bed");
+		Line();
+	}
+	public static void Intro(){
+		System.out.println("Don't stay awake for too long");
+		System.out.println("Don't go to bed");
+		System.out.println("I'll make a cup of coffee for your head");
+		System.out.println("I'll get you up and going out of bed");
+		Line();
+	}
+	public static void Verse1(){
+		System.out.println("Yeah");
+		System.out.println("I don't wanna fall asleep");
+		System.out.println("I don't wanna pass away");
+		System.out.println("I been thinking of our future 'cause I'll never see those days");
+		Line();
+	}
+	public static void Verse2(){
+		System.out.println("I don't know why this has happened");
+		System.out.println("But I probably deserve it");
+		System.out.println("I tried to do my best");
+		System.out.println("But you know that I'm not perfect");
+		Line();
+	}
+	public static void Verse3(){
+		System.out.println("I been praying for forgiveness");
+		System.out.println("You've been praying for my health");
+		System.out.println("When I leave this earth");
+		System.out.println("Hoping you'll find someone else");
+		Line();
+	}
+	public static void Verse4(){
+		System.out.println("'Cause yeah, we still young there's so much we haven't done");
+		System.out.println("Getting married, start a family");
+		System.out.println("Watch your husband with his son");
+		Line();
+	}
+	public static void Verse5(){
+		System.out.println("I wish it could be me");
+		System.out.println("But I won't make it off this bed");
+		System.out.println("I hope I go to heaven");
+		System.out.println("So I see you once again");
+		Line();
+	}
+	public static void Verse6(){
+		System.out.println("My life was kinda short");
+		System.out.println("But I got so many blessings");
+		System.out.println("Happy you were mine");
+		System.out.println("It sucks that it's all ending");
+		Line();
+	}
+	public static void Verse7(){
+		System.out.println("I'm happy that you're here with me");
+		System.out.println("I'm sorry if I tear up");
+		System.out.println("When me and you were younger");
+		System.out.println("You would always make me cheer up");
+		Line();
+	}
+	public static void Verse8(){
+		System.out.println("Taking goofy videos");
+		System.out.println("While walking through the park");
+		System.out.println("You would jump into my arms");
+		System.out.println("Every time you heard a bark");
+		Line();
+	}
+	public static void Verse9(){
+		System.out.println("Cuddle in your sheets");
+		System.out.println("Sang me sound asleep");
+		System.out.println("And sneak out through your kitchen at exactly 1:03");
+		Line();
+	}
+	public static void Verse10(){
+		System.out.println("Sundays went to church");
+		System.out.println("On Mondays watched a movie");
+		System.out.println("Soon you'll be alone");
+		System.out.println("Sorry that you have to lose me");
+		Line();
+	}
+
+	public static void Outro(){
+		System.out.println("And I, don't stay awake for too long");
+		System.out.println("Don't go to bed");
+		System.out.println("I'll make a cup of coffee for your head");
+		System.out.println("I'll get you up and going out of bed");
+		Line();
+	}
+	public static void Credits(){
+		System.out.println("Transcribed by Avery Fernandez");
+		Line();
+	}
+  	public static void main(String[] args) {
+		Title();
+		Intro();
+		Verse1();
+		Verse2();
+		Verse3();
+		Verse4();
+		Verse5();
+		Verse6();
+		Chorus();
+		Verse7();
+		Verse8();
+		Verse9();
+		Verse10();
+		Chorus();
+		Chorus();
+		Outro();
+		Credits();
+	  }
+}
